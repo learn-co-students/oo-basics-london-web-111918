a6218efa7acb3a91505076d620023e72dd956fc8
Sunny <sunny2831@hotmail.co.uk>
2018-11-22 14:34:24 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,56 @@
 # Make your shoe class here!
+class Shoe
+
+ def initialize(brand)
+   @brand = brand
+ end
+
+ def color=(color)
+   @color = color
+ end
+
+ def color
+   @color
+ end
+
+ def size
+ end
+
+ def brand=(brand)
+   @brand = brand
+ end
+
+ def brand
+   @brand
+ end
+
+ def condition=(condition)
+   @condition = condition
+ end
+
+ def condition
+   @condition
+ end
+
+ def material=(material)
+   @material = material
+ end
+
+ def material
+   @material
+ end
+
+ def cobble
+   puts "Your shoe is as good as new!"
+   @condition = "new"
+ end
+
+ def size=(size)
+   @size = 9.5
+ end
+
+ def size
+   @size
+ end
+
+end
